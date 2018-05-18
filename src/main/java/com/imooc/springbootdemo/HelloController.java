3448da4ec741b1a01ac236d3cdd9e5956f5b461b
xiaolei hu <huxiaolei1997@gmail.com>
2018-05-18 15:47:11 +0800

    @Controller, @RequestMapping, @PathVariable, @RequestParam, @GetMapping等注解的使用

--- src/main/java/com/imooc/springbootdemo/HelloController.java
+++ src/main/java/com/imooc/springbootdemo/HelloController.java
@@ -1,12 +1,13 @@
 package com.imooc.springbootdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+//@Controller
+//@ResponseBody
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 //    @Value("${cupSize}")
 //    private String cupSize;
@@ -19,9 +20,12 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String say() {
+    //@RequestMapping(value = {"/say", "/hello"}, method = RequestMethod.GET)
+    @GetMapping(value = "/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
         //return "Hello Spring Boot!";
-        return girlProperties.getCupSize();
+        //return girlProperties.getCupSize();
+        return "id: " + id;
+        //return "index";
     }
 }
